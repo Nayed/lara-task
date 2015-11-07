c7dcbd6bd3c0aaaeb5bab8a6490332988bbad656
Nayed SAID ALI <nayed.saidali@gmail.com>
2015-11-07 23:56:47 +0100

    cleaning routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,32 +4,5 @@ use App\Task;
 use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    $tasks = Task::orderBy('created_at', 'asc')->get();
-
-    return View('tasks', compact('tasks'));
+    return view('welcome');
 });
-
-Route::post('/task', function(Request $request) {
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-
-    return redirect('/');
-});
-
-Route::delete('/task/{id}', function ($id) {
-    Task::findOrFail($id)->delete();
-
-    return redirect('/');
-});
-
